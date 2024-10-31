82d59150aea9dde9b0e3756f1dd3594c709fa4c8
ljm <yhxljm@qq.com>
2024-10-31 13:59:55 +0800

    init

---
+++ cdemo.cpp
@@ -0,0 +1,20 @@
+﻿// cdemo.cpp: 定义应用程序的入口点。
+//
+
+#include "cdemo.h"
+
+using namespace std;
+
+
+int add(int a,int b)
+{
+	return a + b;
+}
+
+int main(int argc,int* argv[])
+{
+	int c = add(1, 2);
+	cout << "c=" << c<<endl;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
